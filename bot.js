26abe6d28d29202c9701bd3f19b529f71be59bd7
AhmadDan <45672879+AhmadDan@users.noreply.github.com>
2018-12-14 21:40:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,26 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '.'
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(,)
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.on('message', msg => {
+  if (msg.content === '#test') {
+    msg.channel.send('Test Successful');
+    msg.channel.sendFile('https://media.giphy.com/media/8GY3UiUjwKwhO/giphy.gif')
+  }
 });
 
 
-
-
-
-
-
-
-
-client.login(process.env.USER_TOKEN);
-client1.login(process.env.USER_TOKEN1);
-client2.login(process.env.USER_TOKEN2);
-client3.login(process.env.USER_TOKEN3);
-client4.login(process.env.USER_TOKEN4);
-client5.login(process.env.USER_TOKEN5);
-client6.login(process.env.USER_TOKEN6);
-client7.login(process.env.USER_TOKEN7);
-client8.login(process.env.USER_TOKEN8);
-client9.login(process.env.USER_TOKEN9);
-client10.login(process.env.USER_TOKEN10);
+client.login(process.env.BOT_TOKEN); 
+client2.login(process.env.BOT_TOKEN2); 
+client3.login(process.env.BOT_TOKEN3); 
+client4.login(process.env.BOT_TOKEN4); 
+client5.login(process.env.BOT_TOKEN5);
+client6.login(process.env.BOT_TOKEN6);
+client7.login(process.env.BOT_TOKEN7);
+client8.login(process.env.BOT_TOKEN8);
